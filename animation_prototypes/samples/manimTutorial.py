c336a2b89af1b059dfe667792ee02e60410481d5
Connor Williams <connorhakan8@gmail.com>
2025-03-06 15:41:43 -0800

    sample manim tutorials
    basically learning how to get the grasp of manim and what it can do

---
+++ animation_prototypes/samples/manimTutorial.py
@@ -0,0 +1,31 @@
+from manim import *
+
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
+
+class ExampleFunctionGraph(Scene):
+    def construct(self):
+        cos_func = FunctionGraph(
+            lambda t: np.power(t,3) - np.power(t,5) + t,
+            color=RED,
+        )
+
+        self.play(Create(cos_func), run_time = 5) 
+            
+            
