9df5c0fb84a61b7170f04faba7ae7053cfae2504
Connor Williams <connorhakan8@gmail.com>
2025-03-09 13:23:12 -0400

    Update powerRule.py
    Added dots, with one dot moving to the other dot. Most of the dot code was made using ChatGPT since I'm not very familiar with manim (like the update from alpha function and the update dot code). I took the code it generated and modified it to my own likings.

--- animation_prototypes/derivatives/powerRule.py
+++ animation_prototypes/derivatives/powerRule.py
@@ -5,25 +5,48 @@ from manim import *
 import math
 
 class Graphing(Scene):
-
+    
     def construct(self):
         #Make graph
         axes = Axes(
-            x_range = [-5, 5, 1],
-            y_range = [-4, 4, 1],
+            x_range = [-4, 4, 1],
+            y_range = [-5, 5, 1],
             axis_config={"include_numbers": True}
         )
 
         graph = axes.plot(lambda x: x**2, color=BLUE)
         graph_label = axes.get_graph_label(graph, label="x^2")
+        label_position = axes.coords_to_point(2, 2)
+        graph_label.next_to(label_position, UP)
+        
         graph2 = axes.plot(lambda x: 2*x, color=BLUE)
         graph_label_2 = axes.get_graph_label(graph, label="2x")
+        graph_label_2.next_to(label_position, UP)
+        
+        def update_dot(mob, alpha):
+            x = interpolate(2, 1, alpha)
+            y = x**2
+            mob.move_to(axes.c2p(x,y))
+        
+        # Create a dot and set its starting position
+        dot = Dot(color=WHITE).move_to(axes.c2p(2, 4))
+        end_dot = Dot(color=RED).move_to(axes.c2p(1,1))
 
         #Display graph
-        self.play(Create(axes))
+        self.play(Create(axes), run_time = 2.5)
+        self.play(Create(graph), run_time = 3)
+        self.play(Create(graph_label), run_time = 0.5)
+        self.wait(0.5)
+        self.add(dot)
+        self.add(end_dot)
+        self.wait(0.5)
+        self.play(UpdateFromAlphaFunc(dot, update_dot, run_time = 2, rate_func = linear))
         self.wait(1)
-        self.play(Create(graph))
-        self.add(graph_label)
-        self.wait(1)
+        self.remove(dot)
+        self.remove(end_dot)
         self.play(Transform(graph, graph2), Transform(graph_label, graph_label_2))
         self.wait(1)
+
+        
+
+        
