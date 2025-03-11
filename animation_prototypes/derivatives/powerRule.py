e48848940e3ce7e825ee750e96de2d2096263262
Connor Williams <connorhakan8@gmail.com>
2025-03-11 17:47:38 -0400

    Experimentation with extending line

--- animation_prototypes/derivatives/powerRule.py
+++ animation_prototypes/derivatives/powerRule.py
@@ -8,6 +8,8 @@ class Graphing(Scene):
     
     def construct(self):
         #Make graph
+        global slope
+        slope = 3
         axes = Axes(
             x_range = [-4, 4, 1],
             y_range = [-5, 5, 1],
@@ -27,10 +29,33 @@ class Graphing(Scene):
             x = interpolate(2, 1, alpha)
             y = x**2
             mob.move_to(axes.c2p(x,y))
-        
+
+        def update_end_dot(mob, alpha):
+            x = interpolate(0.5, 1, alpha)
+            y = x**2
+            mob.move_to(axes.c2p(x,y))
+
+        def update_offscreen_dot(mob, alpha):
+            x = interpolate(2, 1, alpha)
+            y = x**2
+            mob.move_to(axes.c2p(x*5,y*5))
+
         # Create a dot and set its starting position
         dot = Dot(color=WHITE).move_to(axes.c2p(2, 4))
+        offscreen_dot = Dot(color = GREEN).move_to(axes.c2p(4, 10))
         end_dot = Dot(color=RED).move_to(axes.c2p(1,1))
+        end_offscreen_dot = Dot(color = BLACK).move_to(axes.c2p(-2, -8))
+
+
+        
+
+        #end dot: (1,1)
+        #dot: 
+        line = always_redraw(lambda: Line(end_offscreen_dot.get_center(), offscreen_dot.get_center(), color = YELLOW))
+        
+        #axes.c2p(dot.get_center()[0]+5*slope, dot.get_center()[1] + 5*slope)
+        #end_dot.get_center()-(5*slope), dot.get_center()+(5*slope), color = YELLOW))
+        #slope = (dot.get_center()[1] - 1)/(dot.get_center()[0] - 1))
 
         #Display graph
         self.play(Create(axes), run_time = 2.5)
@@ -40,7 +65,10 @@ class Graphing(Scene):
         self.add(dot)
         self.add(end_dot)
         self.wait(0.5)
-        self.play(UpdateFromAlphaFunc(dot, update_dot, run_time = 2, rate_func = linear))
+        self.add(line)
+        self.wait(0.5)
+        self.play(UpdateFromAlphaFunc(dot, update_dot, run_time = 2, rate_func = linear),
+            UpdateFromAlphaFunc(offscreen_dot, update_offscreen_dot, run_time = 2, rate_func = linear))
         self.wait(1)
         self.remove(dot)
         self.remove(end_dot)
