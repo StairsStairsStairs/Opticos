4bcc8736f392c22f4aef56b36a64f3944bc30c44
Busaba-Nicholas <busabaN2014@verizon.net>
2025-03-23 22:58:08 -0400

    Box appears for Area of Boxes

--- animation_prototypes/integrals/renderIntegral.py
+++ animation_prototypes/integrals/renderIntegral.py
@@ -16,6 +16,7 @@ class hello(Scene):
     
     def createIntegralBox(self, x_lower, x_upper, ax, aproxWithLower=True):
         if aproxWithLower:
+            self.var_sum += userInfo.continuous_function(x_lower) * abs(x_upper - x_lower)
             rect = Polygon(
                 ax.c2p(x_lower,0),
                 ax.c2p(x_lower,userInfo.continuous_function(x_lower)),
@@ -26,6 +27,7 @@ class hello(Scene):
                 fill_opacity=1.0
             )
         else:
+            self.var_sum += userInfo.continuous_function(x_upper) * (x_upper - x_lower)
             rect = Polygon(
                 ax.c2p(x_lower,0),
                 ax.c2p(x_lower,function(x_upper)),
@@ -38,6 +40,7 @@ class hello(Scene):
         return rect
 
     def getSetOfNewBoxes(self, ax, numBoxesNow, aproxWithLower=True):
+        self.var_sum = 0
         xVals = self.createIntegralBoxXValues(numBoxesNow)
         allRects = []
         for i in range(numBoxesNow):
@@ -67,13 +70,15 @@ class hello(Scene):
         for box in boxes:
             newBoxes.append(box.copy())
             # turn boxes blue
-            newBoxes[-1].set_fill(BLUE, opacity=1)
-            newBoxes[-1].set_z_index(-1)
+            newBoxes[-1].set_color(userInfo.sum_of_integral_boxes_outline)
+            newBoxes[-1].set_fill(userInfo.sum_of_integral_boxes_fill, opacity=0)
+            newBoxes[-1].set_z_index(1)
         return newBoxes
 
     def construct(self):
-        func = userInfo.continuous_function
+        self.var_sum = 0
         self.camera.background_color = ORANGE
+        func = userInfo.continuous_function
 
         # Get Axis Ranges
         new_x_range = [userInfo.integral_xRange[0] - 1, userInfo.integral_xRange[1] + 1, 0]
@@ -96,12 +101,30 @@ class hello(Scene):
         parabola = ax.plot(lambda x: func(x), new_x_range[0:2], color=BLUE)
         parabola.set_z_index(2)
 
-        # Labels
-        # equation_label = MathTex("y = x^2").to_edge(UP)
+        # Show Box of Area sum
+        Area_box = Square(side_length=3, color=userInfo.box_of_area_under_curve_outline, fill_color=userInfo.box_of_area_under_curve_fill, fill_opacity=1.0)
+        Area_box.move_to(RIGHT * 5.25)
+        Area_box.set_z_index(2)
+        #   Add text to box
+        Area_text = MathTex(r"\text{Area}").scale(1.5).move_to(Area_box.get_center())
+        Area_text.shift(UP * 1)
+        Area_text.set_z_index(2)
+
+        # Area_number = DecimalNumber(self.var_sum, num_decimal_places=4)
+        # Area_MathTex = MathTex("0")
+        # Area_MathTex[0].become(Area_number)
+        # Area_MathTex.move_to(Area_box.get_center()).shift(DOWN * 0.25)
+        # Area_MathTex.set_z_index(2)
+        equation = MathTex("x =")
+        number = DecimalNumber(0, num_decimal_places=0)
+        number.next_to(equation, RIGHT)
+        equation_group = VGroup(equation, number)
+        equation_group.move_to(Area_box.get_center()).shift(DOWN * 0.25)
+        equation_group.set_z_index(2)
         
         # Animate
-        self.play(Create(ax))
-        self.play(Create(parabola))
+        self.play(Create(ax), Create(Area_box))
+        self.play(Create(parabola), Create(Area_text), Create(equation_group))
 
         numBoxes = 5
         currentBoxes = []
@@ -110,12 +133,14 @@ class hello(Scene):
             newBoxesToAdd = self.getSetOfNewBoxes(ax, numBoxes)
             currentBoxes = self.animateAllBoxes(ax, currentBoxes, newBoxesToAdd)
             self.wait(1)
+
             CopiedToShowArea = self.copyAllBoxes(currentBoxes)
             self.add(*[box for box in CopiedToShowArea])
-            finalBox = Square(side_length=1, color=BLUE)
-            finalBox.move_to(RIGHT * 5 + UP * 0)
-            self.play(*[Transform(box, finalBox) for box in CopiedToShowArea], run_time=0.50)
+            self.play(*[box.animate.set_opacity(1) for box in CopiedToShowArea], run_time=0.3)
+            self.play(*[Transform(box, Area_box) for box in CopiedToShowArea], run_time=0.50)
+            print(self.var_sum)
+            self.play(number.animate.set_value(self.var_sum), run_time=0.50)
+            number.set_z_index(2)
             self.remove(*[box for box in CopiedToShowArea])
 
-        # self.play(Create(rect))
         self.wait(1)
--- animation_prototypes/integrals/userInput_Integral.py
+++ animation_prototypes/integrals/userInput_Integral.py
@@ -4,6 +4,12 @@ integral_xRange = [-5, 5]
 integral_box_color_outline = rgb_to_color((0.0, 0.0, 0.0))
 integral_box_color_fill = (rgb_to_color((0.0, 0.0, 1.0)), rgb_to_color((0.0, 1.0, 0.0)), rgb_to_color((1.0, 0.0, 0.0)))
 
+sum_of_integral_boxes_outline = rgb_to_color((0.0, 0.0, 0.0))
+sum_of_integral_boxes_fill = rgb_to_color((0.0, 0.0, 1.0))
+
+box_of_area_under_curve_outline = rgb_to_color((0.0, 0.0, 0.0))
+box_of_area_under_curve_fill = rgb_to_color((0.0, 0.0, 1.0))
+
 function_color = rgb_to_color((0.2, 0.6, 0.8))
 def continuous_function(x):
     return x**3 + 2
