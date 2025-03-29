22ef6443f49009d470df32df21d67570350620a9
Connor Williams <connorhakan8@gmail.com>
2025-03-28 23:56:33 -0400

    Update limitCalc.py

--- animation_prototypes/derivatives/limitCalc.py
+++ animation_prototypes/derivatives/limitCalc.py
@@ -10,9 +10,9 @@ class limitAnimation(Scene):
             axis_config={"include_numbers": True, "include_tip": False}
         )
 
-        func = axes.plot(lambda x: x**2 * (x-3))
+        func = axes.plot(lambda x: (x-1)*(x-2)*(x-3))
 
-        x = ValueTracker(3)
+        x = ValueTracker(1)
         dx = ValueTracker(2)
 
         axes_labels = axes.get_axis_labels(x_label = "x", y_label = "y")
@@ -31,21 +31,34 @@ class limitAnimation(Scene):
             )
         )
 
-        dot1 = always_redraw(
-            lambda: Dot()
-            .move_to(axes.c2p(x.get_value()), func.function(x.get_value()))
-        )
+        #dot1 = always_redraw(
+         #   lambda: Dot().move_to(
+          #  axes.c2p(x.get_value(), func.function(x.get_value()))
+        #)
 
-        dot2 = always_redraw(
-            lambda: Dot()
-            .move_to(
-                axes.c2p(
-                    x.get_value() + dx.get_value(), func.function(x.get_value() + dx.get_value())
-                )
+        dot1 = always_redraw(
+            lambda: Dot().move_to(
+                #axes.c2p(0,0)
+                #print(type(func(x.get_value())))
+                axes.c2p(x.get_value(), func.function(x.get_value())[0])
+                
+                
             )
         )
 
+        dot2 = always_redraw(
+            lambda: Dot().move_to(
+                #axes.c2p(0,0)
+                axes.c2p(x.get_value() + dx.get_value(), func.function(x.get_value() + dx.get_value())[0])
+                #axes.c2p(x.get_value() + dx.get_value()), func.function(x.get_value() + dx.get_value())
+            )
+        )
+
+        print(type(x.get_value()), x.get_value())
+        print(type(func.function(x.get_value())), func.function(x.get_value()))
         self.add(axes, axes_labels, func)
         self.play(Create(VGroup(dot1, dot2, secant)))
         self.play(dx.animate.set_value(0.0001), run_time = 2)
+        self.wait()
+        self.play(x.animate.set_value(1), run_time = 3)
         self.wait(2)
