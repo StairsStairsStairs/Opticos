944d91fe194bbf2a4352e2511ba492e428cbf802
Jayden <jtcarlson05@gmail.com>
2025-04-01 17:09:02 -0400

    Improved readability of code and comments

--- gui/GUI.py
+++ gui/GUI.py
@@ -4,18 +4,37 @@ import cv2
 
 class GUI(object):
     def __init__(self, master):
-        # Layout constants
-        screenResolution = (1600, 900)
+        ##### LAYOUT CONSTANTS #####
+        # Application settings
         master.title("Opticos")
+        root.resizable(False, False)
+        screenResolution = (1600, 900)
+        master.geometry(str(screenResolution[0]) + 'x' + str(screenResolution[1]))
+
+        # Sizing and padding
         buttonWidth = screenResolution[0] // 30
         buttonPadx = buttonWidth // 15
         buttonPady = screenResolution[0] // 100
         objectPackPady = buttonPady
+
+        # Color and font
         frameColor = "Gray"
         buttonFont = ('TkDefaultFont', 15)
         pageFont = ('TkDefaultFont', 12)
-        root.resizable(False, False)
+        
+        ##### MEMBER VARIABLES #####
+        # Set the root Tk() object and parent directory
+        self.parent = master
+        self.directory = Path(__file__).parent
 
+        # Instantiate dictionaries to map frame IDs to frame and their corresponding text
+        self.chapterText = dict()
+        self.frames = dict()
+
+        # Store the ID of the current frame at all times
+        self.currentFrameID = (0, 0)
+
+        ##### HELPER FUNCTIONS #####
         # Create and configure new button object
         def newButton(parent, cmd, buttontext):
             button = Button(parent, command=cmd, text = buttontext, font=buttonFont)
@@ -24,79 +43,100 @@ class GUI(object):
 
         # Create a new information frame for a topic
         def newPage(text, canInputFunction=False):
+            # Main frame of this page
             rootFrame = Frame(master, bg=frameColor, relief=RAISED)
+
+            # Create a sub-frame that holds the text box and scrollbar
             textFrame = Frame(rootFrame, bg=frameColor, relief=RAISED, width=screenResolution[0]//1.4, height=screenResolution[1]//2)
             textFrame.pack(pady=objectPackPady, side=TOP)
+
+            # Create and connect textbox and scrollbar
             pageText = Text(textFrame, font=pageFont, width=150, height=20)
             pageText.insert(END, text)
             pageText.configure(state=DISABLED)
             scrollBar = Scrollbar(textFrame, orient='vertical')
             scrollBar.config(command=pageText.yview)
-            scrollBar.pack(side=RIGHT, fill='y')
             pageText['yscrollcommand'] = scrollBar.set
+
+            # Pack the scrollbar and text into the text frame
+            scrollBar.pack(side=RIGHT, fill='y')
             pageText.pack(side=LEFT)
 
+            # Create an entry box and corresponding sub-frame if needed
             if canInputFunction:
+                # Sub-frame for the entry and its label
                 entryFrame = Frame(rootFrame, bg=frameColor, relief=RAISED, width=200, height=50)
                 entryFrame.pack(side=TOP, pady=(50, 0))
+
+                # Label for the entry frame
                 entryLabel = Label(entryFrame, text="Function: ", pady=2, bg=frameColor)
                 entryLabel.pack(padx=10, side=LEFT)
+
+                # Entry box for the user to input a function
                 functionBox = Entry(entryFrame)
                 functionBox.pack(padx=5, side=RIGHT)
 
+            # Button to play the page's video
             videoButton = newButton(rootFrame, lambda: self.playVideo(str(self.directory/'stockmp4.mp4')), "Play")
             videoButton.pack(pady=objectPackPady, side=TOP)
 
+            # Button to go back and return the new page
             backButton = newButton(rootFrame, lambda: self.switchFrame((self.currentFrameID[0], 0)), 'Back')
             backButton.pack(pady=objectPackPady, side=BOTTOM)
             return rootFrame
 
 
-        # Member variables
-        self.parent = master
-        self.parent.geometry(str(screenResolution[0]) + 'x' + str(screenResolution[1]))
-        self.directory = Path(__file__).parent
-
-        self.chapterText = dict()
-        self.frames = dict()
-        self.currentFrameID = (0, 0)
-        self.mainFrame = Frame(master, bg=frameColor, relief=GROOVE)
 
 
-        # Main frame
-        quitButton = newButton(self.mainFrame, self.terminate, 'Quit')
+
+        ##### MAIN FRAME #####
+        mainFrame = Frame(master, bg=frameColor, relief=GROOVE)
+        self.frames[(0, 0)] = mainFrame
+
+        # Button to terminate the program
+        quitButton = newButton(mainFrame, self.terminate, 'Quit')
         quitButton.pack(pady=objectPackPady, side=BOTTOM)
 
-        testManimButton = newButton(self.mainFrame, self.playManim, 'Manim')
+        # Button to test playing a manim mp4
+        testManimButton = newButton(mainFrame, self.playManim, 'Manim')
         testManimButton.pack(pady=objectPackPady, side=BOTTOM)
 
-        self.frames[(0, 0)] = self.mainFrame
-        subjects = open(self.directory/"exampleText.txt").read().split('-----\n')
-
+        ##### OTHER FRAMES #####
         # Read from text file, creating frames with buttons for each topic.
+        subjects = open(self.directory/"exampleText.txt").read().split('-----\n')
         for i in range(len(subjects)):
+            # Create the menu frame for each subject
             subject = subjects[i]
             frame = Frame(master, bg=frameColor, relief=GROOVE)
             self.frames[(i+1, 0)] = frame
 
+            # Button on the main frame to go to the subject frame
             subjectTitle = subject[:subject.index('\n')].strip()
-            subjectButton = newButton(self.mainFrame, lambda n=i+1: self.switchFrame((n, 0)), subjectTitle)
+            subjectButton = newButton(mainFrame, lambda n=i+1: self.switchFrame((n, 0)), subjectTitle)
             subjectButton.pack(pady=objectPackPady, side=TOP)
+            
+            # Button on the subject frame to go back to the main frame
             backButton = newButton(frame, lambda: self.switchFrame((0, 0)), "Back")
             backButton.pack(pady=objectPackPady, side=BOTTOM)
 
+            # Read every topic in the subject and make a new frame for each
             topics = subject.split('\n\n')[1:]
             for j in range(len(topics)):
+                # Find the title of the topic and the corresponding text for the page
                 topic = topics[j]
                 topicTitle = topic[:topic.index('\n')].strip()
                 text = topic[topic.index('\n'):].strip()
+
+                # Add the new page to the list of frames and text to the list of chapter text
                 self.frames[(i+1, j+1)] = newPage(text, True)
                 self.chapterText[(i+1, j+1)] = text
                 
+                # Button on the subject frame to go to the topic frame
                 topicButton = newButton(frame, lambda n=i+1, m=j+1: self.switchFrame((n, m)), topicTitle)
                 topicButton.pack(pady=objectPackPady, side=TOP) 
 
-        self.mainFrame.pack(expand=True, fill=BOTH)
+        # Pack the main frame so that it is what appears first on startup
+        mainFrame.pack(expand=True, fill=BOTH)
 
 
 
